3c7f888acd5b374af4de031f8cc62414fe538a35
chenlin <1036565912@qq.com>
2020-04-03 20:08:40 +0800

    添加日志记录接口和同步重试方法

--- src/Action/BaseAction.php
+++ src/Action/BaseAction.php
@@ -4,13 +4,21 @@ namespace Chenlin\CanalPhp\Action;
 
 use Com\Alibaba\Otter\Canal\Protocol\Column;
 use Chenlin\CanalPhp\DealInterface\DealInterface;
-
+use Com\Alibaba\Otter\Canal\Protocol\Header;
+use Com\Alibaba\Otter\Canal\Protocol\EventType;
 
 abstract class BaseAction
 {
 
     /** @var array $entrys [包含的都是Entry] */
     protected  $entrys = [];
+
+    /** @var array 事件映射数组 */
+    protected  $event_arr = [
+        EventType::INSERT => '添加',
+        EventType::UPDATE => '更新',
+        EventType::DELETE => '删除'
+    ];
 
     /**
      * 构造函数
@@ -23,9 +31,14 @@ abstract class BaseAction
     }
 
 
-
-    /** 自定义同步数据的业务逻辑 */
-    abstract public function async(DealInterface $deal);
+    /**
+     * 自定义同步数据的业务逻辑
+     * @param DealInterface $deal
+     * @param string $record_log 常规日志记录文件
+     * @param string $error_log 错误日志记录文件
+     * @return mixed
+     */
+    abstract public function async(DealInterface $deal, string $record_log, string $error_log);
 
     /**
      * 转化记录数据格式
@@ -42,4 +55,26 @@ abstract class BaseAction
         }
         return $field_list;
     }
+
+    /**
+     * 日志记录接口[用于记录数据]
+     * @param string $log_path 日志文件路径
+     * @param int $eventType 当前时间类型[insert,update,delete]
+     * @param array $row_data 原始数据[由self::dealColumn 处理得到的数据]
+     * @param Header $header Header头信息 可以获取对应的数据库,表
+     * @return bool
+     */
+    abstract  public function recordLog(string $log_path, int $eventType, array $row_data, Header $header): bool;
+
+    /**
+     * 同步数据失败重试机制
+     * @param int $eventType
+     * @param array $row_data
+     * @param Header $header
+     * @param string $error_log 错误日志文件
+     * @param DealInterface $deal
+     * @param  int $try_num 重试次数
+     * @return void
+     */
+    abstract  public function asyncRetry(int $eventType, array $row_data, Header $header, string $error_log, DealInterface $deal, int $try_num = 3): void;
 }
--- src/Example/DemoAction.php
+++ src/Example/DemoAction.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Chenlin\CanalPhp\Example;
 
@@ -6,9 +7,12 @@ use Chenlin\CanalPhp\Action\BaseAction;
 use Com\Alibaba\Otter\Canal\Protocol\Entry;
 use Com\Alibaba\Otter\Canal\Protocol\EntryType;
 use Com\Alibaba\Otter\Canal\Protocol\EventType;
+use Com\Alibaba\Otter\Canal\Protocol\Header;
 use Com\Alibaba\Otter\Canal\Protocol\RowChange;
 use Com\Alibaba\Otter\Canal\Protocol\RowData;
 use Chenlin\CanalPhp\DealInterface\DealInterface;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class DemoAction extends BaseAction
 {
@@ -16,7 +20,7 @@ class DemoAction extends BaseAction
      * @param DealInterface $deal
      * @throws \Exception
      */
-    public function async(DealInterface $deal)
+    public function async(DealInterface $deal, string $record_log, string $error_log)
     {
         /** @var Entry $entry */
         foreach ($this->entrys as $entry) {
@@ -33,22 +37,189 @@ class DemoAction extends BaseAction
 
             /** @var RowData $rowData */
             foreach ($rowChange->getRowDatas() as $rowData) {
+                $result = false;
+                $row_data = []; //保存原始数据
                 //进行事件判断
                 switch ($evenType) {
-                    case EventType::DELETE:
+                    case EventType::DELETE :
                         echo '删除数据同步'.PHP_EOL;
-                        $result = $deal->delete($this->dealColumn($rowData->getBeforeColumns()));
+                        $row_data = $this->dealColumn($rowData->getBeforeColumns());
+                        $result = $deal->delete($row_data);
                         break;
-                    case EventType::INSERT:
+                    case EventType::INSERT :
                         echo '新增数据同步'.PHP_EOL;
-                        $result = $deal->insert($this->dealColumn($rowData->getAfterColumns()));
+                        $row_data = $this->dealColumn($rowData->getAfterColumns());
+                        $result = $deal->insert($row_data);
                         break;
-                    default:
+                    case EventType::UPDATE :
                         //默认则是修改
                         echo '更新数据同步'.PHP_EOL;
-                        $result = $deal->update($this->dealColumn($rowData->getAfterColumns()));
+                        $row_data = $this->dealColumn($rowData->getAfterColumns());
+                        $result = $deal->update($row_data);
+                }
+                //@tip: 这里存在其他的一些操作事件 要注意防范
+                //如果同步失败 则需要进行重试
+                if (!$result && in_array($evenType, $this->event_arr)) {
+                    //默认重试3次
+                    $this->asyncRetry($evenType, $row_data, $header, $error_log, $deal);
+                }
+
+                //如果同步成功  则进行日志记录
+                if (in_array($evenType, $this->event_arr)) {
+                    $this->recordLog($record_log, $evenType, $row_data, $header);
                 }
             }
         }
+    }
+
+    /**
+     * 简单的一个monolog日志记录方法
+     * @param string $log_path
+     * @param int $eventType
+     * @param array $row_data
+     * @param Header $header
+     * @return bool
+     */
+    public function recordLog(string $log_path, int $eventType, array $row_data, Header $header): bool
+    {
+        // TODO: Implement recordLog() method.
+        //定制日志格式
+        $info = [
+            'database' => $header->getSchemaName(),
+            'table' => $header->getTableName(),
+            'bin-log' => $header->getLogfileName(),
+            'offset' => $header->getLogfileOffset(),
+            'eventType' => $this->event_arr[$eventType],
+            'rowData' => $row_data,
+            'describe' => 'async data success!'
+        ];
+
+        $log = new Logger('async_record');
+        if (!file_exists($log_path)) {
+            throw new \Exception('日志记录文件不存在');
+        }
+
+        $log->pushHandler(new StreamHandler($log_path, Logger::DEBUG));
+        $log->info(json_encode($info, JSON_UNESCAPED_UNICODE));
+        return true;
+    }
+
+
+    /**
+     * 简单的同步失败重试方法
+     * @param int $eventType
+     * @param array $row_data
+     * @param Header $header
+     * @param string $error_log
+     * @param DealInterface $deal
+     * @return void
+     */
+    public function asyncRetry(int $eventType, array $row_data, Header $header, string $error_log, DealInterface $deal, int $try_num = 3): void
+    {
+        // TODO: Implement asyncRetry() method.
+        //第一步判断是否满足符合事件条件
+        if (!in_array($eventType, $this->event_arr)) {
+            return ;
+        }
+
+        //满足  则记录相关的错误信息
+        $log = new Logger('async_fail_log');
+        //判断文件是否存在
+        if (!file_exists($error_log)) {
+            throw new \Exception('错误日志文件不存在');
+        }
+        $log->pushHandler(new StreamHandler($error_log, Logger::DEBUG));
+
+        $error = [
+            'database' => $header->getSchemaName(),
+            'table' => $header->getTableName(),
+            'bin-log' => $header->getLogfileName(),
+            'offset' => $header->getLogfileOffset(),
+            'eventType' => $this->event_arr[$eventType],
+            'rowData' => $row_data,
+            'error' => 'async data fail!'
+        ];
+        //保存首次同步失败记录
+        $log->error(json_encode($error, JSON_UNESCAPED_UNICODE));
+
+        //进行重试
+        $result = false;
+        switch ($eventType) {
+            case EventType::INSERT :
+                $result = $deal->insert($row_data);
+                break;
+            case EventType::DELETE :
+                $result = $deal->delete($row_data);
+                break;
+            case EventType::UPDATE :
+                $result = $deal->update($row_data);
+        }
+
+        //执行成功
+        if ($result) {
+            $success = [
+                'database' => $header->getSchemaName(),
+                'table' => $header->getTableName(),
+                'bin-log' => $header->getLogfileName(),
+                'offset' => $header->getLogfileOffset(),
+                'eventType' => $this->event_arr[$eventType],
+                'rowData' => $row_data,
+                'describe' => 'first retry  async success!!!!'
+            ];
+            $log->info(json_encode($success, JSON_UNESCAPED_UNICODE));
+            return ;
+        }
+
+        $dict = [
+            1 => 'first',
+            2 => 'second',
+            3 => 'third',
+            4 => 'fourth',
+            5 => 'fifth',
+            6 => 'sixth'
+        ];
+        //如果失败 则还要继续重试 从第二次开始
+        for ($i = 2; $i <= $try_num; $i++) {
+            //进行重试
+            $result = false;
+            switch ($eventType) {
+                case EventType::INSERT :
+                    $result = $deal->insert($row_data);
+                    break;
+                case EventType::DELETE :
+                    $result = $deal->delete($row_data);
+                    break;
+                case EventType::UPDATE :
+                    $result = $deal->update($row_data);
+            }
+
+            //重试成功
+            if ($result) {
+                $success = [
+                    'database' => $header->getSchemaName(),
+                    'table' => $header->getTableName(),
+                    'bin-log' => $header->getLogfileName(),
+                    'offset' => $header->getLogfileOffset(),
+                    'eventType' => $this->event_arr[$eventType],
+                    'rowData' => $row_data,
+                    'describe' => $dict[$i].' retry  async success!!!!'
+                ];
+
+                $log->info(json_encode($success), JSON_UNESCAPED_UNICODE);
+                break;
+            } else {
+                $error = [
+                    'database' => $header->getSchemaName(),
+                    'table' => $header->getTableName(),
+                    'bin-log' => $header->getLogfileName(),
+                    'offset' => $header->getLogfileOffset(),
+                    'eventType' => $this->event_arr[$eventType],
+                    'rowData' => $row_data,
+                    'error' => $dict[$i].' retry data fail!'
+                ];
+                $log->emergency(json_encode($error), JSON_UNESCAPED_UNICODE);
+            }
+        }
+
     }
 }
